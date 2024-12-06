941a5ede2c71d391ca964a8a756f0319858e472e
Wayyuanyuan <155795538+Wayyuanyuan@users.noreply.github.com>
2024-12-06 17:10:54 +0800

    Delete make_answer directory

--- make_answer/chat/chat_invoker.py
+++
@@ -1,3 +0,0 @@
-class ChatInvoker:
-    def chat(self, msg: str, *args, **kwargs) -> str:
-        raise NotImplementedError("Subclasses must implement this method")
--- make_answer/works/TCMeEE_score.py
+++
@@ -1,9 +0,0 @@
-from make_answer.chat.chat_invoker import ChatInvoker
-
-
-def tcmeee(data: dict, llm: ChatInvoker) -> dict:
-    prompt = f"{data['question']}"
-    response = llm.chat(prompt)
-    data["answer"] = response
-
-    return data
